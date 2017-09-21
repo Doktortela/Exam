40081c1a9ff5549f45516d7ecd5847d3d9e456ae
Doktortela <dok2xxx@ukr.net>
2017-09-21 19:41:50 +0300

    second

--- Exam/Exam/Program.cs
+++ Exam/Exam/Program.cs
@@ -10,6 +10,31 @@ namespace Exam
     {
         static void Main(string[] args)
         {
+            string s = "Привет мир";
+            char c = 'в';
+            int i = s.WordCount(c);
+            Console.WriteLine(i);
+            Console.WriteLine(s.WordAdding());
+            Console.ReadLine();
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static string WordAdding(this string str)
+        {
+            return str+" . PS: made by vova";
+        }
+
+        public static int WordCount(this string str, char c)
+        {
+            int counter = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == c)
+                    counter++;
+            }
+            return counter;
         }
     }
 }
